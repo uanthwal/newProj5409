de5f051e68f4d2cea24a2409670b14a51c274bd4
Upendra Parsad <upendra@Upendras-MacBook-Pro.local>
2020-02-20 09:41:52 -0400

    Updated code to read input from file

--- Factorial.java
+++ Factorial.java
@@ -1,10 +1,23 @@
-class Factorial{  
- public static void main(String args[]){  
-  int i,fact=1;  
-  int number=15;//It is the number to calculate factorial    
-  for(i=1;i<=number;i++){    
-      fact=fact*i;    
-  }    
-  System.out.println("Factorial of "+number+" is: "+fact);    
- }  
-}  
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+class Factorial {
+	public static void main(String args[]) throws FileNotFoundException, IOException {
+		File file = new File("input.txt");
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		    	int i, fact = 1;
+				int number = Integer.parseInt(line);
+				for (i = 1; i <= number; i++) {
+					fact = fact * i;
+				}
+				System.out.println("Factorial of " + number + " is: " + fact);
+		    }
+		}
+		
+	}
+}
--- Fibonacci.java
+++ Fibonacci.java
@@ -1,15 +1,28 @@
-class Fibonacci{  
-public static void main(String args[])  
-{    
- int n1=0,n2=1,n3,i,count=100;    
- System.out.print(n1+" "+n2);//printing 0 and 1    
-    
- for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
- {    
-  n3=n1+n2;    
-  System.out.print(" "+n3);    
-  n1=n2;    
-  n2=n3;    
- }    
-  
-}}  
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+class Fibonacci {
+	public static void main(String args[]) throws NumberFormatException, IOException {
+
+		File file = new File("input.txt");
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				int n1 = 0, n2 = 1, n3, i, count = Integer.parseInt(line);
+				System.out.println();
+				System.out.print(n1 + " " + n2);// printing 0 and 1
+				for (i = 2; i < count; ++i)
+				{
+					n3 = n1 + n2;
+					System.out.print(" " + n3);
+					n1 = n2;
+					n2 = n3;
+				}
+
+			}
+		}
+
+	}
+}
